8c6bb0d5ac53b81b1457745bdba5f8a9af500040
Your Name <deasis_franklin@yahoo.com>
2021-01-03 13:32:18 +0800

    Travis.yml added

---
+++ tutorial_android_application/src/test/java/com/techyourchance/unittesting/screens/questiondetails/QuestionDetailsControllerTest.java
@@ -0,0 +1,42 @@
+package com.techyourchance.unittesting.screens.questiondetails;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentCaptor.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class QuestionDetailsControllerTest {
+
+    // region constants
+
+    // endregion constants
+
+    // region helper fields
+
+    // enregion helper fields
+
+    QuestionDetailsController SUT;
+
+    @Before
+    public void setup() throws Exception {
+
+    }
+
+    // region helper methods
+
+    //endregion helper methods
+
+    //region helper classes
+
+    //endregion helper classes
+}
